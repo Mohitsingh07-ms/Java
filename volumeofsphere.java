68cbaa299878c80e79f64edc4d02d5c96cdfbdb1
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-06-29 15:38:35 +0530

    Delete volumeofsphere.java

--- volumeofsphere.java
+++
@@ -1,7 +0,0 @@
-public class volumeofsphere {
-    public static void main(String[] args) {
-        double r = 4.8;
-        double v = (4*3.14*r*r*r)/3;
-        System.out.println("volume of sphere with radius "+r+" is "+v);
-    }
-}
