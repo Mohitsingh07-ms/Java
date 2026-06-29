a69061e1c2161525c86e7b8581afaf02378846d1
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-06-29 15:35:44 +0530

    CHange

---
+++ MODULE 6/volumeofsphere.java
@@ -0,0 +1,7 @@
+public class volumeofsphere {
+    public static void main(String[] args) {
+        double r = 4.8;
+        double v = (4*3.14*r*r*r)/3;
+        System.out.println("volume of sphere with radius "+r+" is "+v);
+    }
+}
