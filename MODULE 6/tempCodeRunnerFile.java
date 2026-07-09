7f4101b5107d788cb95789250a05fd0b6e36d4eb
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-07-09 20:34:48 +0530

    input

--- MODULE 6/tempCodeRunnerFile.java
+++ MODULE 6/tempCodeRunnerFile.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class halfofgiveninteger {
+public class tempCodeRunnerFile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double y = sc.nextDouble();
