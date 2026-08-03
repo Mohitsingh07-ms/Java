4e986249b0c6c40c84fa1f9fe8a82bf964632117
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-08-03 21:42:59 +0530

    five

---
+++ MODULE 6/Fiveorthree.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Fiveorthree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        if(n%5 == 0 || n%3 == 0) System.out.println("Divisible by  5 or 3");
+        else System.out.println("Not divisible by 5 or 3");
+    }
+}
