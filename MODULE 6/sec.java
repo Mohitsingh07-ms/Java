71c4926a2a23a10817d73e435cf1f3e81aafbf99
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-08-16 22:03:58 +0530

    triangle

--- MODULE 6/sec.java
+++ MODULE 6/sec.java
@@ -1,5 +1,5 @@
 public class sec {
     public static void main(String[] args) {
-        System.err.println("Hello World");
+        System.out.println("Hello World");
     }
 }
---
+++ MODULE 6/validtriangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class validtriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st side : ");
+        int a = sc.nextInt();
+        System.out.print("Enter 2nd side : ");
+        int b = sc.nextInt();
+        System.out.print("Enter 3rd side : ");
+        int c = sc.nextInt();
+        if((a+b)>c && (b+c)>a && (c+a)>b) System.out.println("valid triangle");
+        else System.out.println("invalid triangle");
+        }
+    }
