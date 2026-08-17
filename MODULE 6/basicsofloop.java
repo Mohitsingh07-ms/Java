ef0bb3b74793a59fe0bf598e4c087adce01fd4b9
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-08-17 20:59:43 +0530

    loops

--- MODULE 6/basicsofloop.java
+++ MODULE 6/basicsofloop.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class basicsofloop {
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
             System.out.println("Hello World");
         }
     }
