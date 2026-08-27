768a3b8d6145124f4a0e616586d2afcbfe36a5b7
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-08-27 20:32:22 +0530

    dowhile

---
+++ MODULE 6/whileloop.java
@@ -0,0 +1,19 @@
+public class whileloop {
+    public static void main(String[] args) {
+        // for(int i=1;i<=10;i++){
+        // System.out.println(i);
+        // }
+
+        int i=1;
+        while(i<=8){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+//    we can make for loop into while loop
+//    int i=1;
+//    for(;i<=13;){
+//    System.out.println(i);
+//    i++;
+//    }
