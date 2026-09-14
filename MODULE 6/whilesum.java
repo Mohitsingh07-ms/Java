5796f1215e34239760f5331154d4eb976492fb08
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-09-14 20:05:27 +0530

    loop sum

---
+++ MODULE 6/whilesum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class whilesum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+
+int sum =0;
+while(n!=0){
+        int ld = n%10;
+        sum += ld;
+        n /=10;
+}
+System.out.println("sum = " +sum);
+    }
+}
