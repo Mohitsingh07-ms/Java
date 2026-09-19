343e193c1644d94e873f5ed387ea2ab1e9aebe06
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-09-19 22:34:15 +0530

    triangle3

--- MODULE 6/numbertriangle.java
+++ MODULE 6/numbertriangle.java
@@ -5,7 +5,7 @@ Scanner sc = new Scanner(System.in);
 System.out.print("Enter n : ");
 int n = sc.nextInt();
 for(int i=1;i<=n;i++){
-    for(int j=1;j<i;j++){
+    for(int j=1;j<=i;j++){
         System.out.print(j + " ");
     }
     System.out.println();
