c689bd1ac2b3d7f7bbb6d98a6a61e1392add8e1b
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-09-24 20:27:43 +0530

    pyramid

--- MODULE 6/starpyramid.java
+++ MODULE 6/starpyramid.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class numbersquare4 {
+public class starpyramid {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n :");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(i + " ");
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*"+ " ");
         }
         System.out.println();
         }
