38fe4cf066c975869e1bf72b72507bed70ebf384
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-09-25 11:35:16 +0530

    numberpyramid

---
+++ MODULE 6/numberpyramid.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class numberpyramid {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+
+            // spaces
+          //  for(int j = 1; j <= n-i; j++) {
+            //    System.out.print("  ");
+            //}
+
+            // increasing numbers
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            // decreasing numbers
+            for(int j = i-1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
