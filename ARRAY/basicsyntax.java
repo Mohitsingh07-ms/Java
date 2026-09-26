c7e47d8f659e7cbf0b784f57e347c10175ddc179
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-09-26 14:56:36 +0530

    array

--- ARRAY/basicsyntax.java
+++ ARRAY/basicsyntax.java
@@ -7,6 +7,11 @@ arr[1] = 20;
 arr[2] = 30;
 arr[3] = 40;
 arr[4] = 50;
-System.out.println(arr[0]);
+// output of array elements
+System.out.print(arr[0] + " ");
+System.out.print(arr[1] + " ");
+System.out.print(arr[2] + " ");
+System.out.print(arr[3] + " ");
+System.out.print(arr[4] + " ");
     }
 }
