2ea252daf202ebe15b51f3cdc240873ef16c68e2
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-09-26 15:29:09 +0530

    two

---
+++ ARRAY/initialise2.java
@@ -0,0 +1,13 @@
+public class initialise2 {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,6,7,8};
+        // int[] arr = {10,20,30,40,-50};
+        // for(int i=0;i<7;i++){
+        // }
+        int n = arr.length;
+        System.out.println(n);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
