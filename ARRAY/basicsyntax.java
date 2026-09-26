ede191b73dbc1d871c60412029d06c58d05edbe1
Mohitsingh07-ms <singhmohit65689@gmail.com>
2026-09-26 14:52:43 +0530

    array

---
+++ ARRAY/basicsyntax.java
@@ -0,0 +1,12 @@
+public class basicsyntax {
+    public static void main(String[] args) {
+int[] arr = new int[5];
+// initialising individual elements
+arr[0] = 10;
+arr[1] = 20;
+arr[2] = 30;
+arr[3] = 40;
+arr[4] = 50;
+System.out.println(arr[0]);
+    }
+}
